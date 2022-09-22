a222d3f5e9834787b5e8f5cbcf97c945f8344259
dhaist <dhaist@emich.edu>
2022-09-21 23:01:43 -0400

    app sent into production

---
+++ app/src/main/java/com/techstack/techstacksetup/List.java
@@ -0,0 +1,83 @@
+package com.techstack.techstacksetup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.techstacksetup.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class List extends AppCompatActivity {
+    Button returnToMainButton;
+    //Firebase instance. This connects to the database
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    //connects to a collection in the database called users
+    private CollectionReference usersRef = db.collection("names");
+    private ArrayList<String> nameArray;
+    ListView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+        CollectionReference c = db.collection("names");
+        nameArray = new ArrayList<>();
+        view = (ListView)findViewById(R.id.list);
+        view.setBackgroundColor(Color.WHITE);
+
+
+        // button that returns to main activity
+        returnToMainButton = (Button)findViewById(R.id.returnToMain);
+        returnToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(List.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+   }
+    //loads the names from the database and displays them
+    //invoked when the user clicks on the fetch button
+    public void loadName(View v){
+        usersRef.get()//get method grabs all the information from the collection users
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    //onSuccess method makes sure get method works
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+                        //For loop which goes through every piece of data in the collection
+                        for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            //Creates a new user. Sends info User.class
+                            User user = documentSnapshot.toObject(User.class);
+                            //set a variable for the name of the user using getName from the user class
+                            String name = user.getName();
+                            //Data is a large string with a new line after every entry
+                            data += "Name: " + name + "\n";
+                            nameArray.add(name);
+                        }
+                        //Displays the data to the screen
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                                android.R.layout.simple_list_item_1, nameArray);
+
+                        view.setAdapter(arrayAdapter);
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/techstack/techstacksetup/MainActivity.java
@@ -0,0 +1,98 @@
+package com.techstack.techstacksetup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.techstacksetup.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Creates a firebase instance
+    private FirebaseFirestore vDB = FirebaseFirestore.getInstance();
+    //CollectionReference creates a reference to the collection we want to access
+    private CollectionReference userRef = vDB.collection("names");
+
+
+    Button nameButton,nextbtn;
+    EditText helloNameInput;
+    TextView helloName,label;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameButton = (Button)findViewById(R.id.button);
+        nextbtn = (Button)findViewById(R.id.nextBtn);
+        helloNameInput  = (EditText)findViewById(R.id.editTextTextPersonName);
+        helloName = (TextView)findViewById(R.id.helloName);
+        label= (TextView) findViewById(R.id.warning);
+        final long[] mLastClickTime = {0};
+
+        helloNameInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloNameInput.setText("");
+            }
+        });
+
+
+        nameButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view) {
+                        if(helloNameInput.getText().toString().isEmpty()){
+                            label.setTextColor(Color.RED);
+                            label.setText("Can't Leave it Blank");
+                            return;
+                        }
+                        label.setText("");
+                        if (SystemClock.elapsedRealtime() - mLastClickTime[0] < 10000) {
+                            helloName.setText("Button cool down. Please do not spam the save button.");
+                            return;
+                        }
+                        mLastClickTime[0] = SystemClock.elapsedRealtime();
+                            String name = helloNameInput.getText().toString();
+                            User newUser = new User(name);
+                        helloName.setText("Hello, " + name);
+
+                        //sends data to the database
+                        vDB.collection("names").add(newUser);
+
+                        try {
+                            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+                    }
+                });
+
+
+        /**
+         * onClick function to the next activity
+         */
+        nextbtn.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Intent intent = new Intent(MainActivity.this, List.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+
+}
